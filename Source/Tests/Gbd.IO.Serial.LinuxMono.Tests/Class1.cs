d6c5b59e48e7817681592dfbd8801daf40485c41
grbd <garlicbready@googlemail.com>
2017-01-16 01:15:40 +0000

    Tidy ups

---
+++ Source/Tests/Gbd.IO.Serial.LinuxMono.Tests/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Gbd.IO.Serial.LinuxMono.Tests
+{
+    public class Class1
+    {
+        [Fact]
+        public void Test2() { }
+    }
+}
